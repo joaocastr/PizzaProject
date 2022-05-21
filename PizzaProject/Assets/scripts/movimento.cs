ddf8fd2f245c8d0f16d1bdfdd6b4c6cf89063924
j4um <joaovictorc2506@gmail.com>
2022-05-21 10:35:19 -0300

    movento pronto
    Setas movem o personagem vertical e horizontalmente

--- PizzaProject/Assets/scripts/movimento.cs
+++ PizzaProject/Assets/scripts/movimento.cs
@@ -17,13 +17,18 @@ public class movimento : MonoBehaviour
     void Update()
     {
         transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
-        if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f );
+
+        if(Vector3.Distance(transform.position, movePoint.position) <= .05f)
         {
-            movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
-        }
-        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) ;
-        {
-            movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+
+            if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1f );
+            {
+                movePoint.position += new Vector3(Input.GetAxisRaw("Horizontal"), 0f, 0f);
+            }
+            if (Mathf.Abs(Input.GetAxisRaw("Vertical")) == 1f) ;
+            {
+                movePoint.position += new Vector3(0f, Input.GetAxisRaw("Vertical"), 0f);
+            }
         }
     }
 }
